3a72ace516f317021e4967015b95aa8fd207d88b
Nikita Voino <ankapkekw@gmail.com>
2024-04-19 13:02:06 +0300

    Lab_1 and lab_2

---
+++ Lab_2/Task_1/Task1.cs
@@ -0,0 +1,36 @@
+﻿string answ;
+do
+{
+    string a;
+    long x;
+    Console.WriteLine("Введите целоче число: ");
+    while(true)
+    {   
+
+        a = Console.ReadLine();
+        bool result = long.TryParse(a, out x);
+        if (!result)
+        {
+            Console.WriteLine("Вводите только числа.");
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    }
+    //string a = Console.ReadLine();
+    switch (a[a.Length - 1])
+    {
+        case '7':
+            Console.WriteLine("Это число заканчивается на 7.");
+            break;
+        default:
+            Console.WriteLine("Число не заканчивается на 7.");
+            break;
+    }
+    Console.WriteLine("Хотите ли вы продолжить? (Продолжить/Закончить)");
+    answ = Console.ReadLine();
+    Console.WriteLine("------------------------------------------------------------------------------------------------------------------------"); 
+} while (answ == "Продолжить" || answ == "Да" || answ == "Yes" || answ == "Y" || answ == "y" || answ == "1");
+
---
+++ Lab_2/Task_2/Task2.cs
@@ -0,0 +1,63 @@
+﻿string answ;
+const int b = 123; 
+do
+{
+    long x, y;
+    Console.WriteLine("Введите координаты x, y: ");
+    while (true)
+    {
+        string xString = Console.ReadLine();
+        bool result = long.TryParse(xString, out x);
+        if (!result)
+        {
+            Console.WriteLine("Вводите только числа.");
+            continue;
+        }
+
+        string yString = Console.ReadLine();
+        bool result1 = long .TryParse(yString, out y);
+        if (!result1)
+        {
+            Console.WriteLine("Вводите только числа.");
+            continue;
+        }
+        if(result && result1)
+        {
+            break;
+        }
+        //int x = Convert.ToInt32(xString);
+        //int y = Convert.ToInt32(Console.ReadLine()); 
+    }
+    double dist = Math.Sqrt(Math.Pow(x - 0, 2) + Math.Pow(y - 0, 2));
+    int otvet = 0;
+    if (dist == 15 || dist == 25)
+    {
+        otvet = 1;
+    }
+    else if (dist > 15 && dist < 25)
+    {
+        otvet = 2;
+    }
+    else if (dist < 15 || dist > 25)
+    {
+        otvet = 3;
+    }
+
+    switch (otvet)
+    {
+        case 1:
+            Console.WriteLine("Точка лежит на границе.");
+            break;
+        case 2:
+            Console.WriteLine("Точка лежит вне заштрихованной области.");
+            break;
+        case 3:
+            Console.WriteLine("Точка лежит в заштрихованной зоне.\n");
+        break;
+    }
+
+        Console.WriteLine("Хотите ли вы продолжить? (Продолжить/Закончить)");
+        answ = Console.ReadLine();
+        Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+} while (answ == "Продолжить" || answ == "Да" || answ == "Yes" || answ == "Y" || answ == "y" || answ == "1");
+
